e6d07d23a683c1c126aa21159f0d353bbecca1ce
IvanBzhaniya <ivan.bzhaniya@gmail.com>
2023-06-16 17:48:26 +0300

    ex1.1

--- Program.cs
+++ Program.cs
@@ -7,12 +7,15 @@ Console.WriteLine("input number ");
 int number = Convert.ToInt32(Console.ReadLine());;
 if (number > 99 && number < 1000)
 {
-    int numbertemp = number / 10;
-    int digit2 = numbertemp % 10;
+    int digit2 = number / 10 % 10;
     Console.WriteLine("second digit is " + digit2);
 }
 else
 {
     Console.WriteLine("input 3-digit number");
 }
+
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
 
