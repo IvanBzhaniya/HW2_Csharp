29df147da7a6ad818a167f6af50c4385f43ee185
IvanBzhaniya <ivan.bzhaniya@gmail.com>
2023-06-16 17:00:57 +0300

    ex1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+*/
+
+Console.WriteLine("input number ");
+int number = Convert.ToInt32(Console.ReadLine());;
+if (number > 99 && number < 1000)
+{
+    int numbertemp = number / 10;
+    int digit2 = numbertemp % 10;
+    Console.WriteLine("second digit is " + digit2);
+}
+else
+{
+    Console.WriteLine("input 3-digit number");
+}
+
